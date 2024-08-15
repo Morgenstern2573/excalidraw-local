b1e74a1310f9a37c32809240592740b5d8d668b8
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-08-15 09:54:33 +0100

    add user table to schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS Drawings;
 DROP TABLE IF EXISTS Collections;
+DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Collections(
   Name TEXT UNIQUE, 
@@ -13,4 +14,10 @@ CREATE TABLE Drawings(
   Data TEXT, 
   FOREIGN KEY(CollectionID) REFERENCES Collections(ID),
   PRIMARY KEY(Name, CollectionID)
+);
+
+CREATE TABLE Users(
+  Name TEXT,
+  Email TEXT PRIMARY KEY,
+  PasswordHash TEXT NOT NULL
 );
