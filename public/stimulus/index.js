d1eaaf01736eec1287e4669c81236be19a0ced4d
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-08-21 20:04:07 +0100

    bug fixes

--- public/stimulus/index.js
+++ public/stimulus/index.js
@@ -4,39 +4,25 @@ import {
 } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js";
 window.Stimulus = Application.start();
 
-Stimulus.register(
-  "toggleVisibility",
-  class ToggleVisibility extends Controller {
-    static targets = ["content"];
+class ToggleVisibility extends Controller {
+  static targets = ["content"];
 
-    constructor() {
-      super();
-
-      this.listener = null;
-    }
-
-    connect() {
-      this.contentTarget.addEventListener(
-        "htmx:beforeOnLoad",
-        this.hideContent
-      );
-
+  connect() {
+    console.log(this);
+    this.contentTarget.addEventListener("htmx:beforeOnLoad", () => {
       this.hideContent();
-    }
+    });
 
-    showContent() {
-      this.contentTarget.classList.remove("!hidden");
-    }
-
-    hideContent() {
-      this.contentTarget.classList.add("!hidden");
-    }
-
-    disconnect() {
-      this.contentTarget.removeEventListener(
-        "htmx:beforeOnLoad",
-        this.hideContent
-      );
-    }
+    this.hideContent();
   }
-);
+
+  showContent() {
+    this.contentTarget.classList.remove("!hidden");
+  }
+
+  hideContent() {
+    this.contentTarget.classList.add("!hidden");
+  }
+}
+
+Stimulus.register("toggleVisibility", ToggleVisibility);
