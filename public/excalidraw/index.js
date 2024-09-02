98d25081edd44a22fa6689b08cb08727f40defaf
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-09-02 07:16:26 +0100

    bug fixes

--- public/excalidraw/index.js
+++ public/excalidraw/index.js
@@ -163,6 +163,7 @@ function initExcalidraw() {
     htmx.ajax("POST", "/app/update-drawing-data", {
       values: form,
       swap: "none",
+      source: "#app",
     });
   }, 1000);
 
@@ -203,32 +204,32 @@ function initExcalidraw() {
           return;
         }
 
-        // try {
-        //   fetch(`/app/get-users-at-drawing?drawingID=${DRAWING_ID}`)
-        //     .then((response) => response.json())
-        //     .then((resp) => {
-        //       let users = JSON.parse(resp);
+        try {
+          fetch(`/app/get-users-at-drawing?drawingID=${DRAWING_ID}`)
+            .then((response) => response.json())
+            .then((resp) => {
+              let users = JSON.parse(resp);
 
-        //       const collaborators = new Map();
-        //       for (let user of users) {
-        //         collaborators.set(user.userID, {
-        //           username: user.username,
-        //           pointer: {
-        //             x: parseFloat(user.xPos),
-        //             y: parseFloat(user.yPos),
-        //             tool: "pointer",
-        //           },
-        //         });
-        //       }
+              const collaborators = new Map();
+              for (let user of users) {
+                collaborators.set(user.userID, {
+                  username: user.username,
+                  pointer: {
+                    x: parseFloat(user.xPos),
+                    y: parseFloat(user.yPos),
+                    tool: "pointer",
+                  },
+                });
+              }
 
-        //       excalidrawAPIRef.current.updateScene({
-        //         collaborators: collaborators,
-        //       });
-        //     });
-        // } catch (err) {
-        //   console.log("error getting user positions");
-        //   console.error(err);
-        // }
+              excalidrawAPIRef.current.updateScene({
+                collaborators: collaborators,
+              });
+            });
+        } catch (err) {
+          console.log("error getting user positions");
+          console.error(err);
+        }
       }, 1000);
 
       return cleanup;
